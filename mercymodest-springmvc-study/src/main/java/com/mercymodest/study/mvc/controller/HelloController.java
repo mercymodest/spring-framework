51db7c47582e62251856b7b83d0d1c784ca3a944
mercymodest <zgh995@aliyun.com>
2023-08-17 14:35:03 +0800

    feat: - edit HellController  add method parameter - index.jsp

--- mercymodest-springmvc-study/src/main/java/com/mercymodest/study/mvc/controller/HelloController.java
+++ mercymodest-springmvc-study/src/main/java/com/mercymodest/study/mvc/controller/HelloController.java
@@ -1,8 +1,11 @@
 package com.mercymodest.study.mvc.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 /**
@@ -10,7 +13,7 @@ import java.time.format.DateTimeFormatter;
  * @version V1.0.0
  * @create 08-03 2:48
  */
-@RestController
+@Controller
 public class HelloController {
 
 	public HelloController() {
@@ -18,7 +21,8 @@ public class HelloController {
 	}
 
 	@GetMapping("/hello")
-	public String hello() {
-		return "hello"+ DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(java.time.LocalDateTime.now());
+	public String hello(String name, @RequestParam(value = "account") String account, HttpSession session) {
+		session.setAttribute("info",String.format("Hello,%s,your account is %s time: %s", name, account, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now())));
+		return "/";
 	}
 }
---
+++ mercymodest-springmvc-study/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mercymodest
+  Date: 2023/8/17
+  Time: 13:02
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>index</title>
+</head>
+<body>
+   <h1>${sessionScope.get("info")}</h1>
+</body>
+</html>
